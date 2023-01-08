ca468d5244638e518d1ba3f2f4f74b69f8b69dfc
prcstak <g.marsel2002@yandex.ru>
2023-01-08 18:12:43 +0300

    add account project

--- src/tic-tac-toe/src/api/httpClient.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const BASE_URL = process.env.API_URL ?? "http://localhost:xxxx/"
-
-const httpClient = axios.create({
-    baseURL: BASE_URL
-});
-
-export default httpClient;
