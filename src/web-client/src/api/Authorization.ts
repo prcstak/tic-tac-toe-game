60242d273a0dbc0874105c98169a31460f77f56f
prcstak <g.marsel2002@yandex.ru>
2023-01-13 13:57:53 +0300

    aaa

--- src/web-client/src/api/Authorization.ts
+++ src/web-client/src/api/Authorization.ts
@@ -4,17 +4,25 @@ class Authorization {
     static async SignUp() {
         return httpClient.post("" , { }, {
 
-        })
+        }).then((response => {
+            localStorage.setItem("token", response.data);
+        }))
     }
 
     static async LogIn() {
         return httpClient.post("" , { }, {
             
-        })
+        }).then((response => {
+            localStorage.setItem("token", response.data);
+        }))
     }
 
     static isLogged() {
-        return true; //..   
+        let token = localStorage.getItem("token");   
+        if (token) {
+            return true;
+        }
+        return false;
     }
 }
 
